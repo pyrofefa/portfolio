a16afbc7635a26ec345180d1088c7e4d344d0e67
Siafeson <siafeson@Mac-mini-de-Siafeson.local>
2024-01-05 14:56:22 -0700

    Lang test

---
+++ src/i18n/ui.ts
@@ -0,0 +1,27 @@
+export const routes = {
+    en: {
+      'services': 'leistungen',
+    },
+    es: {
+      'services': 'prestations-de-service',
+    },
+}
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.about': 'About',
+      'nav.experience': 'Experience',
+    },
+    es: {
+      'nav.about': 'Sobre Mi',
+      'nav.experience': 'Experiencia',
+    },
+  } as const;
+
+  
